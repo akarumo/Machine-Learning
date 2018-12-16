db6e10708b810345f8db534c8c7ac4787d11132d
akarumo <akarumo@yahoo.com>
2018-12-16 11:14:00 +0530

    missed committing this earlier

--- 1-DecisionTress.py
+++ 1-DecisionTress.py
@@ -21,5 +21,20 @@ print(titanic_train.shape)
 print(titanic_train.info())
 print(titanic_train.describe())
 
-# consider non-categorical and non-missing data columns
+# consider non-categorical and non-missing data columns on X-axis and Survived on Y-axis
+x_titanic_train = titanic_train[['Pclass', 'SibSp', 'Parch']] #X-Axis
+y_titanic_train = titanic_train['Survived'] #Y-Axis
 
+#Build the decision tree model using defaults. The default algorithm criterion used is Gini 
+dt = tree.DecisionTreeClassifier()
+dt.fit(x_titanic_train, y_titanic_train)
+
+# Now predict the outcome using decision tree model built above
+#Read the Test Data
+titanic_test = pd.read_csv("C:\\Users\\aditya\\Documents\\0 Data Science\\Titanic\\test.csv")
+x_test = titanic_test[['Pclass', 'SibSp', 'Parch']]
+#Use .predict method on Test data using the model which we built
+titanic_test['Survived'] = dt.predict(x_test) 
+os.getcwd() #To get current working directory
+os.chdir("C:\\Users\\aditya\\Documents\\0 Data Science\\Titanic\\")
+titanic_test.to_csv("prediction0.csv", columns=['PassengerId','Survived'], index=False)
